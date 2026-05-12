491f1640f57b9bc2e10a811189b1268d4cb75849
chintanmehta21 <chintan.98.mehta@gmail.com>
2026-05-12 16:33:56 +0530

    chore(tests): drop tautological mint_signup_only_user wrapper

--- tests/v2/fixtures/__init__.py
+++ tests/v2/fixtures/__init__.py
@@ -1,13 +1,11 @@
 from tests.v2.fixtures.kastens import MintedKasten
 from tests.v2.fixtures.users import (
     MintedUser,
-    mint_signup_only_user,
     mint_test_user_with_workspaces,
 )
 
 __all__ = [
     "MintedKasten",
     "MintedUser",
-    "mint_signup_only_user",
     "mint_test_user_with_workspaces",
 ]
